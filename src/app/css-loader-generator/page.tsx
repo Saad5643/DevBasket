ffc128b83e663afd06ee4d1d990911294474bbd1
Saad Naved <saadmc555@gmail.com>
2025-07-16 18:51:24 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/css-loader-generator/page.tsx
+++ src/app/css-loader-generator/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
