53b763641671a66b22f15a0d762bbce73f45cf37
Saad Naved <saadmc555@gmail.com>
2025-07-20 09:59:35 +0000

    ✅ Fix This in Next.js If you're using next-sitemap, you likely forgot to

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://devbasket.site',
+  siteUrl: 'https://devbasket.site', // ← Your actual domain here
   generateRobotsTxt: true,
-  generateIndexSitemap: false,
 };
